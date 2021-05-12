a1bb75ef3ab5e220087c0ffc68d2a88b1d2f2a1e
rlund760 <robdlundberg@gmail.com>
2021-05-12 10:18:06 -0700

    front end

---
+++ client/src/pages/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer(){
+    return(<footer className="bg-warning text-center text-black">
+        <p>&copy; Robert</p>
+    </footer>)
+}
+
+
+
+export default Footer;
---
+++ client/src/pages/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header(){
+    return(<header className="bg-warning text-center text-black">
+        <h1>Google Books API</h1>
+    </header>)
+}
+
+
+
+export default Header;
